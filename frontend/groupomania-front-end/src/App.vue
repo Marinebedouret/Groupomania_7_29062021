0fc418a2c786a8d345241f75860d9a4d138c86ed
Marinebedouret <marine.galloo@gmail.com>
2021-10-22 12:18:32 +0200

    fichier app

---
+++ frontend/groupomania-front-end/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+ <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  margin-top: 5%;
+  margin-right: 5%;
+  margin-left: 5%;
+}
+
+a {
+  text-decoration: none;
+  }
+.home{
+  img { 
+    width: 20%;
+    float: none;
+    }
+}
+</style>
+
+
