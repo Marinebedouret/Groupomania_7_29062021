76a825cd650826e1ee145aad0cf9ee1d2518b0b6
Marinebedouret <marine.galloo@gmail.com>
2021-10-22 12:07:43 +0200

    fichier route pour les posts

---
+++ backend/routes/post.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth');
+const postCtrl = require('../controllers/post');
+
+router.post('/create',auth,multer, postCtrl.createdPost);
+router.get('/', auth,postCtrl.getAllPosts);
+router.get('/:id',auth,postCtrl.getOnePost);
+router.delete('/:id',auth,postCtrl.deletePost);
+router.put('/:id', auth, postCtrl.modifyPost);
+
+
+module.exports = router;
