a6eb388c44245f84635e6a69dd57c7e797410289
Marinebedouret <marine.galloo@gmail.com>
2021-10-22 12:15:56 +0200

    fichier profil user

---
+++ frontend/groupomania-front-end/src/views/Profil.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="Profil">
+        <headers/>
+        <userProfil/>
+    </div>
+    
+</template>
+<script>
+import headers from '../components/header.vue';
+import userProfil from '../components/userProfil.vue';
+
+export default {
+    name: 'Profil',
+    components:{
+        headers,
+        userProfil
+    }
+}
+
+</script>
