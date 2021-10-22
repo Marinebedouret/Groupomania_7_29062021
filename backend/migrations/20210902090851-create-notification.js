271f385c188d32977ff52f3fafee6796129844bf
Marinebedouret <marine.galloo@gmail.com>
2021-10-22 12:02:21 +0200

    fichier migrations des notifications

---
+++ backend/migrations/20210902090851-create-notification.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Notifications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_notification: {
+        type: Sequelize.INTEGER
+      },
+      id_users: {
+        type: Sequelize.INTEGER
+      },
+      
+      text_notification: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Notifications');
+  }
+};
