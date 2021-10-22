ebe708a22bc3f9959623bd5425138282c00c09f7
Marinebedouret <marine.galloo@gmail.com>
2021-10-22 12:03:02 +0200

    fichier migration des commentaires

---
+++ backend/migrations/20210902091726-create-comment.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      id_users: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id_users',
+        }
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+    
+      picture: {
+        type: Sequelize.STRING
+      },
+      id_post: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  }
+};
