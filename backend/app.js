0951c12cc29e4449237688604fbfa9d72b36b79f
Marinebedouret <marine.galloo@gmail.com>
2021-10-22 11:54:34 +0200

    app.js

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+//dotenv masque les informations de connexion à la base de données à l'aide de variable d'environnement
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet');
+
+
+const app = express();
+
+const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
+const commentRoutes = require('./routes/comment');
+const likeRoutes = require('./routes/like');
+const notificationRoutes = require('./routes/notification');
+
+
+//CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+//Middleware pour les images
+app.use('images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+app.use('/api/like', likeRoutes);
+
+
+
+module.exports = app;
