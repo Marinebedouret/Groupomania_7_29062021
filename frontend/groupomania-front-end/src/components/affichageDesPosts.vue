01e4ecd25bd5aaab941341b8e54eda99c5a0f7e2
marinebedouret <69307460+Marinebedouret@users.noreply.github.com>
2021-11-08 15:48:23 +0100

    Add files via upload

--- frontend/groupomania-front-end/src/components/affichageDesPosts.vue
+++ frontend/groupomania-front-end/src/components/affichageDesPosts.vue
@@ -21,10 +21,30 @@
             
         </div>
         <!--<like :id_post="post.id_post" :id_users="post.id_users"></like>-->
-        <comment :id_post="post.id_post" :id_users="post.id_users"></comment>
-    </div>
-    
 
+            <!-- Affichage des commentaire + création des commentaires-->
+
+        <div class="bloccomments">
+            <textarea type="text" rows="2" id="text" name="newComment" class="from-control" v-model="text" placeholder="Ajouter un commentaire..." ></textarea>
+            <button v-on:click="createComment(post.id_post)" type="submit" id="send_comment"> Publier </button>
+        </div>
+        <div>
+            <i  @click="displayComment(post.id_post)"  class="far fa-comment-alt"></i>
+            <span v-if="post.comments.length > 0" class="item_length">{{post.comments.length}}</span>
+        </div>
+        <div>
+            <div class="displayComment" v-for="comment in comments" :key="comment.id_comment">
+                <div class="comment_infos"> 
+                    <span>Posté le {{ dateFormat(comment.created_at)}} </span>
+                    <span>{{comment.user.first_name}} {{comment.user.name}}</span>
+                </div>
+            
+                <div class="viewText">
+                     <p>{{comment.text}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 </template>
@@ -32,15 +52,14 @@
 <script>
 import axios from 'axios';
 import moment from 'moment';
-import comment from '../components/affichageDesCommentaires.vue'
+
 
 
 
 export default {
     name: "viewAllPosts",
     components: {
-        comment
-  
+
 
     },
     data(){
@@ -49,20 +68,22 @@ export default {
             name: "",
             job:"",
             isAdmin:"",
-            id_post: "",
-            picture: "",
+            id_post:"",
             id_users: localStorage.getItem('id_users'),
+            picture: "",
             posts: [],
             post: "",
+            comment: "",
+            comments: [],
+            text:"",
+            
      
         }
     
     },
     mounted(){
-        console.log(localStorage)
         
         axios.get(`http://localhost:3000/api/post`,
-     
         {
             headers: {
                 'Content-Type': 'application/json',
@@ -75,10 +96,52 @@ export default {
             console.log(this.posts)
             
         })
-        .catch(error => console.log(error))
-
+        .catch(error => console.log(error));
     },
     methods:{
+        
+          //Affichage des commentaires
+        displayComment(id_post){
+            axios.get("http://localhost:3000/api/comment/" + id_post,
+
+            {
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                    'Content-Type': 'application/json'
+                }
+    
+            })
+        .then(response => {
+            console.log(response)
+            this.comments = response.data;
+            console.log(this.comments)
+        })
+        .catch(error => console.log(error))
+         
+        },
+        //Création d'un commentaire
+        createComment(id_post) {
+            axios.post(`http://localhost:3000/api/comment/` + id_post,
+            {
+                id_users: this.id_users,
+                text: this.text,
+                id_post
+
+            },
+            {
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem("token"),
+                    'Content-Type' : 'application/json'
+                }
+
+            })
+            .then(() => {
+                window.location.reload()
+
+               
+            })
+            .catch(error => console.log(error));
+        },
         
         //Affiche la date de publication au bon format
         dateFormat(date){
@@ -88,12 +151,13 @@ export default {
         },
         //supprimer un post si isAdmin ou non 
         deletePost(id_post){
-           axios.delete(`http://localhost:3000/api/post/` + id_post,
-    
+           axios.delete(`http://localhost:3000/api/post/delete/` + id_post,
+
            {
                headers:{
+                   'Authorization': 'Bearer ' + localStorage.getItem("token"),
                    'Content-Type': 'application/json',
-                   'Authorization': "Bearer " + localStorage.getItem('token')
+                   
                     }
            })
              .then(response => {
